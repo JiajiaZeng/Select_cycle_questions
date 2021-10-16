0532b93844fc08060f91b46a423d64e5d6536c40
JiajiaZeng <3034177851@qq.com>
2021-10-16 15:35:10 +0800

    chapter 3

---
+++ src/Check_ISBN.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Check_ISBN {
+
+
+	/*private static int getNumLenght(int num){//本来想来个判断是不是9位数来着，结果不太行，就先注释掉了
+        int i=0;
+        while(num!=0) {
+        	num=num/10;
+        	i++;
+        }
+        return i;
+    }*/
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        Scanner scan=new Scanner(System.in);
+        
+        System.out.print("Enter the first 9 digits of an ISBN as integer: ");
+        int num=scan.nextInt();
+        
+        /*if(getNumLength(num)!=9) {
+        	System.out.println("Length error!");
+        }*/
+        
+        int numOri=num;
+        int i=1;
+        int total=0;
+        for(i=1;i<=9;i++) {
+        	total+=(num/(int)Math.pow(10,(9-i)))*i;
+        	num=num%(int)Math.pow(10, (9-i));
+        }
+        int finalNum;
+        finalNum=total%11;
+        if(finalNum==10) {
+        	if(numOri/(int)Math.pow(10, 8)==0) {
+        		System.out.println("The ISBN-10 number is 0"+numOri+"X");
+        	}else {
+        		System.out.println("The ISBN-10 number is "+numOri+"X");
+        	}
+        }else {
+        	if(numOri/(int)Math.pow(10, 8)==0) {
+        		System.out.println("The ISBN-10 number is 0"+numOri+finalNum);
+        	}else {
+        		System.out.println("The ISBN-10 number is "+numOri+finalNum);
+        	}
+        }
+	}
+
+}
---
+++ src/Random_month.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+/**
+ * Returns a pseudo-random number between min and max, inclusive.
+ * The difference between min and max can be at most
+ * <code>Integer.MAX_VALUE - 1</code>.
+ *
+ * @param min Minimum value
+ * @param max Maximum value.  Must be greater than min.
+ * @return Integer between min and max, inclusive.
+ * @see java.util.Random#nextInt(int)
+ */
+
+public class Random_month {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+        int month=randInt(1,12);
+        
+        switch (month) {
+            case 1:System.out.println("January");break;
+            case 2:System.out.println("February");break;
+            case 3:System.out.println("March");break;
+            case 4:System.out.println("April");break;
+            case 5:System.out.println("May");break;
+            case 6:System.out.println("June");break;
+            case 7:System.out.println("July");break;
+            case 8:System.out.println("August");break;
+            case 9:System.out.println("September");break;
+            case 10:System.out.println("October");break;
+            case 11:System.out.println("November");break;
+            case 12:System.out.println("December");break;
+        }
+        
+	}
+	
+	public static int randInt(int min, int max) {
+
+	    // NOTE: Usually this should be a field rather than a method
+	    // variable so that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+}
+
+
+
