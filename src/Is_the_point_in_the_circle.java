aa1c6e4415de35ae97511105702b9e6990702bd0
JiajiaZeng <3034177851@qq.com>
2021-10-19 00:29:05 +0800

    3-22

---
+++ src/Is_the_point_in_the_circle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Is_the_point_in_the_circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter point coordinates(x,y): ");
+		int x=input.nextInt();
+		int y=input.nextInt();
+		if(Math.pow((Math.pow(x, 2)+Math.pow(y, 2)),0.5)<=10) {
+			System.out.println("Point ("+x+","+y+") in circle");
+		}
+		else {
+			System.out.println("The point ("+x+","+y+") is not in the circle");
+		}
+	}
+
+}
