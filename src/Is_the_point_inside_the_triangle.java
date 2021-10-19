765fd35e888eec934536605e9f9e526fe5f4d6fe
JiajiaZeng <3034177851@qq.com>
2021-10-19 15:52:51 +0800

    3-27

---
+++ src/Is_the_point_inside_the_triangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Is_the_point_inside_the_triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter point coordinates(x,y): ");
+		float x=input.nextInt();
+		float y=input.nextInt();
+		if(x>=0 && y>=0 && 0.5*x+y<=100) {
+			System.out.println("The point ("+x+","+y+") is inside the triangle");
+		}
+		else {
+			System.out.println("The point ("+x+","+y+") is not in the triangle");
+		}
+	}
+
+}
