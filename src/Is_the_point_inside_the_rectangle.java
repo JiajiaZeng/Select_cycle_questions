b8205c1e90ca8006261796501dece3b58c165ca6
JiajiaZeng <3034177851@qq.com>
2021-10-19 15:55:43 +0800

    3-23:int->float

--- src/Is_the_point_inside_the_rectangle.java
+++ src/Is_the_point_inside_the_rectangle.java
@@ -6,8 +6,8 @@ public class Is_the_point_inside_the_rectangle {
 		// TODO Auto-generated method stub
 		Scanner input=new Scanner(System.in);
 		System.out.print("Enter point coordinates(x,y): ");
-		int x=input.nextInt();
-		int y=input.nextInt();
+		float x=input.nextInt();
+		float y=input.nextInt();
 		if(Math.abs(x)<=5 && Math.abs(y)<=2.5) {
 			System.out.println("Point ("+x+","+y+") is in rectangle");
 		}
