15f1ef3707baee8b523469f7d98e3da70168e351
JiajiaZeng <3034177851@qq.com>
2021-10-19 16:12:57 +0800

    3-22

--- src/Is_the_point_in_the_circle.java
+++ src/Is_the_point_in_the_circle.java
@@ -6,10 +6,10 @@ public class Is_the_point_in_the_circle {
 		// TODO Auto-generated method stub
 		Scanner input=new Scanner(System.in);
 		System.out.print("Enter point coordinates(x,y): ");
-		float x=input.nextInt();
-		float y=input.nextInt();
+		float x=input.nextFloat();
+		float y=input.nextFloat();
 		if(Math.pow((Math.pow(x, 2)+Math.pow(y, 2)),0.5)<=10) {
-			System.out.println("Point ("+x+","+y+") in circle");
+			System.out.println("Point ("+x+","+y+") is in circle");
 		}
 		else {
 			System.out.println("The point ("+x+","+y+") is not in the circle");
